91ea7b168d73da1429de24297722150860ad05e1
jjieunyun <lesson0186@gmail.com>
2022-05-19 23:28:45 +0900

    init project

--- React/Habit-tracker/src/index.js
+++ React/Habit-tracker/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './app';
+import App from './app';
 import '@fortawesome/fontawesome-free/js/all.js';
-import SimpleHabit from './components/simpleHabit';
+//import SimpleHabit from './components/simpleHabit';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //엄격모드
   <React.StrictMode>
-    <SimpleHabit />
+    <App />
   </React.StrictMode>
 );
 
