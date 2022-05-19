4ebcdec686850c72d44b9625a62e7372ff79ce3c
jjieunyun <lesson0186@gmail.com>
2022-05-20 00:06:07 +0900

    유뷰트 셋업

---
+++ React/test/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Button1 from './components/button1';
+import Button2 from './components/button2';
+
+function App() {
+  return (
+    <div className="App">
+      <Button1/>
+      <Button2/>
+    </div>
+  );
+}
+
+export default App;
